8c4405956bc84c9c45a3ecfeeec86dbd7ccadeac
DanielCovautl <covault08@gmail.com>
2025-03-03 17:53:18 -0500

    add dependences

---
+++ app/src/main/java/com/example/commerce/data/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.commerce.data
+
+//https://www.we-areunited.com:8080/cafeteria/getItems
+const val BASE_URL = "https://www.we-areunited.com:8080/cafeteria/"
+const val ENDPOINT = "getItems"
+const val BODY = "body"
